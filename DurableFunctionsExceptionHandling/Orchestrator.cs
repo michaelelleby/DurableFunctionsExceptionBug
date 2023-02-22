51125b166f66659ab69c826c49bf08c884892341
Michael Elleby <michael@softwareahead.dk>
2023-02-22 11:05:33 +0100

    Demonstrate issue

---
+++ DurableFunctionsExceptionHandling/Orchestrator.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace DurableFunctionsExceptionHandling
+{
+    public static class Orchestrator
+    {
+        [FunctionName("Orchestrator")]
+        public static async Task<List<string>> RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var outputs = new List<string>();
+
+            // Replace "hello" with the name of your Durable Activity Function.
+            try
+            {
+                outputs.Add(await context.CallActivityAsync<string>(nameof(SayHello), "Tokyo"));
+            }
+            catch (FunctionFailedException exception)
+            {
+                if (exception.InnerException is HttpRequestException httpException)
+                {
+                    outputs.Add($"Caught HttpRequestException with statusCode: '{httpException.StatusCode}' and message: {httpException.Message}");
+                }
+                else
+                {
+                    outputs.Add($"Caught exception of type: {exception.InnerException?.GetType().FullName}");
+                }
+            }
+
+            return outputs;
+        }
+
+        [FunctionName(nameof(SayHello))]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
+        {
+            throw new HttpRequestException($"Saying hello to {name}.", default, HttpStatusCode.BadRequest);
+        }
+
+        [FunctionName("Orchestrator_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("Orchestrator", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
